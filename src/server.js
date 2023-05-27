83bd99b9cfdf39eb8aa0f4ef792eaac02924b52d
arga.christian <arga.christian@ui.ac.id>
2023-05-28 02:24:33 +0700

    Update: add body-parser

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import userRoutes from "./routes/user.route.js";
+import genreRoutes from "./routes/genre.route.js";
 import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 dotenv.config();
 const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Successful Response");
 });
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/genres", genreRoutes);
 
 const startServer = () => {
   const PORT = process.env.PORT || 8080;
