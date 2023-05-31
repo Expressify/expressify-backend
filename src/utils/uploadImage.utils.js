e25fef221eaf27758e594d3650de8cc948e65044
arga.christian <arga.christian@ui.ac.id>
2023-05-31 09:10:13 +0700

    Update: change filename

--- src/utils/uploadImage.utils.js
+++ src/utils/uploadImage.utils.js
@@ -1,5 +1,6 @@
 import { storage } from "../configs/cloudBucket.config.js";
 import * as dotenv from "dotenv";
+import * as path from "path";
 
 dotenv.config();
 
@@ -8,8 +9,11 @@ const bucket = storage.bucket(process.env.BUCKET_NAME);
 export const uploadImage = (file) =>
   new Promise((resolve, reject) => {
     const { originalname, buffer } = file;
+    var ext = path.extname(originalname);
 
-    const blob = bucket.file(originalname.replace(/ /g, "_"));
+    const blob = bucket.file(
+      Date.now().toString() + "-" + (Math.random() * 10e17).toString() + ext
+    );
     const blobStream = blob.createWriteStream({
       resumable: false,
     });
