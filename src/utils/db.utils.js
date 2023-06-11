8ab8c6f5295dac647a241dcac96d95a8415c49ab
arga.christian <arga.christian@ui.ac.id>
2023-06-11 09:41:58 +0700

    Fix: stopping the connection when finished query

--- src/utils/db.utils.js
+++ src/utils/db.utils.js
@@ -3,6 +3,7 @@ import createPool from "../configs/db.config.js";
 async function query(sql, params) {
   const pool = await createPool();
   const data = await pool.query(sql, params);
+  pool.end();
   return data;
 }
 
